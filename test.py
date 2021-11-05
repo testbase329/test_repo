e3dbfafc7eec4504b52187baa5792bd506b8ba41
testbase329 <93765410+testbase329@users.noreply.github.com>
2021-11-05 12:52:08 +0100

    delete

--- test.py
+++
@@ -1,9 +0,0 @@
-def createTxt():
-print(getWindowsUsername())
-with open('C:\Users\'+getWindowsUsername()+'\Desktop\test.txt', 'w') as f:
-f.write('Hello World!')
-def getWindowsUsername():
-import getpass
-return getpass.getuser()
-if __name__ == '__main__':
-createTxt()
