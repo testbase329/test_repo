a8c9ede0965358800033bd6cd41aaed918f2612d
testbase329 <93765410+testbase329@users.noreply.github.com>
2021-11-05 12:46:27 +0100

    commit

---
+++ test.py
@@ -0,0 +1,9 @@
+def createTxt():
+print(getWindowsUsername())
+with open('C:\Users\'+getWindowsUsername()+'\Desktop\test.txt', 'w') as f:
+f.write('Hello World!')
+def getWindowsUsername():
+import getpass
+return getpass.getuser()
+if __name__ == '__main__':
+createTxt()
